a56975b7302ecf2363114581acae4106e39f4281
Bram Strack van Schijndel <bramsvs@gmail.com>
2019-04-15 15:24:49 +0200

    uncomment vids, add pdf-export.pdf

--- prelim/youtube/download.py
+++ prelim/youtube/download.py
@@ -3,16 +3,16 @@
 from subprocess import Popen
 
 VIDS = [
-    # "3v0GQX5Ptyc",
-    # "8qMpE-hr2X0",
-    # "shgeDmgA36I",
-    # "-rJi_78Ef_E",
-    # "D-988dJbSJU",
-    # "oWj3ivhWr40",
-    # "kq_Hgg5J-rQ", # fail compilation (big)
-    # "bsHryqnvyYA", # ETH indoors
-    # "ek0FrCaogcs", # ETH outdoors
-    # "2nDEEw3VjNU", # DJI Inspire 2
+    "3v0GQX5Ptyc",
+    "8qMpE-hr2X0",
+    "shgeDmgA36I",
+    "-rJi_78Ef_E",
+    "D-988dJbSJU",
+    "oWj3ivhWr40",
+    "kq_Hgg5J-rQ", # fail compilation (big)
+    "bsHryqnvyYA", # ETH indoors
+    "ek0FrCaogcs", # ETH outdoors
+    "2nDEEw3VjNU", # DJI Inspire 2
     "ScYDOqFGOhk", # Delft Bebop FTC
 ]
 
